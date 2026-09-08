d33ce57be7b66b1ede646e5ce34b20ecd2aa5167
muzamil-ashiq <bmuzamil38@gmail.com>
2026-09-08 21:23:43 +0530

    fix(script): resolve argument parsing and script path expansion in start_mission.sh

--- scripts/start_mission.sh
+++ scripts/start_mission.sh
@@ -19,12 +19,12 @@ if [ -f "$HOME/.local/bin/distrobox" ]; then
     DISTROBOX="$HOME/.local/bin/distrobox"
 fi
 
-$DISTROBOX enter gracemo-harmonic -- bash -c '
+$DISTROBOX enter gracemo-harmonic -- bash -c "
     source /opt/ros/humble/setup.bash
-    source "$1/ros2_ws/install/setup.bash"
-    export DISPLAY="${2:-:0}"
-    export WAYLAND_DISPLAY="${3:-}"
+    source '$WS_DIR/install/setup.bash'
+    export DISPLAY='${DISPLAY:-:0}'
+    export WAYLAND_DISPLAY='${WAYLAND_DISPLAY:-}'
     export QT_X11_NO_MITSHM=1
-    shift 3
-    python3 "$1/scripts/mission_visualizer.py" "$@"
-' _ "$DIR" "${DISPLAY:-:0}" "${WAYLAND_DISPLAY:-}" "$@"
+    python3 '$DIR/scripts/mission_visualizer.py' \"\$@\"
+" _ "$@"
+
